476551e70ff70ec84db1786e44add92994c18740
JackRipper01 <francohernandezpiloto@gmail.com>
2022-11-03 00:11:52 -0400

    lol

--- GameLibrary/Class1.cs
+++ GameLibrary/Class1.cs
@@ -1,5 +1,34 @@
 ﻿namespace GameLibrary;
-public class Class1
-{
 
+public struct Cards
+{
+    public string name;
+    public int health { get; set; }
+    public string element;
+    public int ATK;
+
+    /*
+
+
+    SI QUIERES VER LO QUE HACE EL MR INCREIBLE, COMETE ERRORES ;)
+
+
+
+    */
+
+    public Cards(string name, int health, string element, int ATK)
+    {
+        this.name = name;
+        this.health = health;
+        this.element = element;
+        this.ATK = ATK;
+    }
 }
+public static class AttackAction
+{
+    public static void Attack(Cards A, Cards B)//A ataca a B
+    {
+        A.health -= B.ATK;
+    }
+}
+
