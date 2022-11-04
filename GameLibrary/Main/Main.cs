49ac40524a58d53871c9b1c5ac9ebf037affc091
JackRipper01 <francohernandezpiloto@gmail.com>
2022-11-04 00:24:22 -0400

    GodUpdate

--- GameLibrary/Main/Main.cs
+++ GameLibrary/Main/Main.cs
@@ -9,16 +9,33 @@ List<Card> playerBfield = new List<Card>();
 
 for (int turn = 1; turn <= 20; turn++)
 {
+    Player player = new Player();
+    Player enemyPlayer = new Player();
+    List<Card> playerField = new List<Card>();
+    List<Card> enemyField = new List<Card>();
+    if (turn % 2 != 0)//turno par=playerA playing,turno impar=playerB playing
+    {
+        player = playerA;
+        enemyPlayer = playerB;
+        playerField = playerAfield;
+        enemyField = playerBfield;
+    }
+    else
+    {
+        player = playerB;
+        enemyPlayer = playerA;
+        playerField = playerBfield;
+        enemyField = playerAfield;
+    }
     int playerCardSelection = int.Parse(Console.ReadLine());
-    int oponentCardSelection = int.Parse(Console.ReadLine());
+    int targetCardSelection = int.Parse(Console.ReadLine());
     if (playerCardSelection == -1)
     {
-        playerA.energy += 2;
+        PlayerActions.IncreaseEnergy(player, 2);
         continue;
     }
-    AttackAction.Attack(playerAfield[playerCardSelection], playerBfield[oponentCardSelection]);
-    playerA.energy -= 2;
-    if (playerBfield[oponentCardSelection].health <= 0)
-        playerBfield.RemoveAt(oponentCardSelection);
-
+    AttackAction.Attack(playerField[playerCardSelection], enemyField[targetCardSelection]);
+    PlayerActions.DecreaseEnergy(player, 2);
+    if (enemyField[targetCardSelection].health <= 0)
+        enemyField.RemoveAt(targetCardSelection);
 }
