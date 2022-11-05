d664e209e1e97c512d4c1af3d9dba40156d944dd
JackRipper01 <francohernandezpiloto@gmail.com>
2022-11-05 19:07:49 -0400

    two new methods at Game.cs :)

--- GameLibrary/Objects/Game.cs
+++ GameLibrary/Objects/Game.cs
@@ -5,6 +5,8 @@ public class Game
     public const int BoardSize = 4;
     public const int MaxNumberOfTurns = 20;
     public int TurnCounter { get; set; }
+    public int finalScorePlayer1 = 0;
+    public int finalScorePlayer2 = 0;
     private Player Player1;
     private Player Player2;
     public Dictionary<Player, List<Card>> Board;
@@ -51,4 +53,23 @@ public class Game
     public bool HasLost(Player player)
         => player.Deck.Count == 0 && player.Hand.Count == 0 && Board[player].Count == 0;
 
+
+    public int GetSurvivorScored(Player player)//retorna la suma de la vida las cartas(en campo) del player(para q sirve? 
+    //pues se me ocurrio una idea,q hacer si se acaban los turnos?quien ganaria?pues gana el q tenga mayor suma total de la 
+    //vida se sus cartas,lo cual seria el score)
+    {
+        int score = 0;
+        for (int i = 0; i < Board[player].Count; i++)
+        {
+            score += Board[player][i].Health;
+        }
+        return score;
+    }
+    public void UpdatePlayerBoard(List<Card> playerBoard)//remueve del campo toda carta con vida negativa
+    {
+        for (int i = 0; i < playerBoard.Count; i++)
+        {
+            if (playerBoard[i].Health <= 0) playerBoard.RemoveAt(i);
+        }
+    }
 }
