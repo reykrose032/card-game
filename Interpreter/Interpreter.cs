32da19245b3854ceac7aab9f8791347f9c7d0d47
Kevin <kevinperezrose@gmail.com>
2022-12-10 12:47:11 -0800

    wip interpreter

---
+++ Interpreter/Interpreter.cs
@@ -0,0 +1,59 @@
+﻿namespace Interpreter;
+
+public enum TokenType
+{
+    Integer,
+    Plus,
+    EOF
+}
+
+public class Token
+{
+    public TokenType Type { get; }
+    public int Value { get; }
+
+    public Token(TokenType type)
+    {
+        Type = type;
+    }
+
+    public Token(TokenType type, int value)
+    {
+        Type = type;
+        Value = value;
+    }
+}
+
+public class Interpreter
+{
+    public string Text { get; }
+    private int _position;
+
+    public Interpreter(string text)
+    {
+        Text = text;
+        _position = 0;
+    }
+
+    public Token GetNextToken()
+    {
+        if (_position > Text.Length - 1)
+            return new Token(TokenType.EOF);
+        
+        var currentChar = Text.ElementAt(_position);
+        if (char.IsDigit(currentChar))
+        {
+            _position++;
+            return new Token(TokenType.Integer);
+        }
+
+        if (currentChar == '+')
+        {
+            _position++;
+            return new Token(TokenType.Plus);
+        }
+
+        throw new Exception("Error parsing input");
+    }
+
+}
